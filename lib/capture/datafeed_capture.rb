8ba2bf7838df73bac129446753d5e7f22c64ee1b
Sillson <Sillson88@gmail.com>
2015-07-18 00:04:47 -0700

    names and dates extractable from FPC report, fish logic to come

--- lib/capture/datafeed_capture.rb
+++ lib/capture/datafeed_capture.rb
@@ -12,15 +12,34 @@ class DataFeedCapture
 		doc.xpath("//item").count == 14 ? true : false
 	end
 
+
+
 	def parse_to_db(capture)
 		
 		# Parse FPC report
 		capture.css('item').each do |node|
-			puts node.css('title').inner_text
+			name_and_date = node.css('title').inner_text
+			name = extract_name(name_and_date)
+			date = extract_date(name_and_date)
+			
 			puts node.css('description').inner_text
  		end
 
 		
+	end
+
+
+	private 
+    
+    # Refactor this verboseness
+	def extract_date(str)
+		regex = /\d{1,2}\/\d{1,2}\/\d{4}/
+		str["regex"]
+	end
+
+	def extract_name(str)
+		regex = /\d{1,2}\/\d{1,2}\/\d{4}/
+		str.gsub(regex, '').strip!
 	end
 
 end
