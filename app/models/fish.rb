eb05e5f5787f81162551bb8df973e7f0a350ec21
Sillson <Sillson88@gmail.com>
2015-07-18 17:16:16 -0700

    added date validation, may break scraper when running a feed where data has changed for one dam and not the other. will confirm and add rescue to scraper if need be

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,3 +1,3 @@
 class Fish < ActiveRecord::Base
-	validates :name, presence: true, uniqueness: {case_sensitive: false}
+	validates :name, presence: true, uniqueness: true
 end
--- app/models/fish_count.rb
+++ app/models/fish_count.rb
@@ -2,6 +2,8 @@ class FishCount < ActiveRecord::Base
 	belongs_to :fish 
 	belongs_to :dam
 
+	validates :date, presence: true, uniqueness: true
+
 	def format_date
     	self.date.strftime("%B %d, %Y")
     end
