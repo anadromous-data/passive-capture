cda11e664e62f1e078c62c146ea869c53612166e
Sillson <Sillson88@gmail.com>
2015-07-22 19:48:44 -0700

    cleaned up polyline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require home_map_polyline
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
