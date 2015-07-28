b65d3a46e7f548199682f3ff6b7823d302828c55
Sillson <Sillson88@gmail.com>
2015-07-28 00:02:26 -0700

    fixed heroku noko issue and proceeded a bit with the legacy parser until kyle came home with red wine

--- lib/capture/legacy_capture.rb
+++ lib/capture/legacy_capture.rb
@@ -8,17 +8,25 @@ class LegacyCapture
 		crude = CSV.read("public/#{csv}.csv")
 		fix_csv(crude)
 		@report = CSV.read("public/ephemeral.csv", headers:true)
-		@dam = (Dam.where("LOWER(name) LIKE ?", dam_name.downcase).first).id
+		@dam = extract_dam(dam_name)
 	end
 
 	def parse_to_db
 		puts 'Hello'
 		puts "#{@report}"
 
-	end
+		@report.each do |fish_run|
+			@date = extract_date(fish_run["Date"])
 
-	#date
-	# Date.strptime(csv, '%m/%d/%Y')
+			begin
+				@new_count = FishCount.new(dam_id: @dam_id, count_date: @date, fish_id: @fish_id, count: @fish_count)
+				@new_count.save!
+			rescue ActiveRecord::RecordInvalid => invalid
+				puts invalid.record.errors
+			end
+
+		end
+	end
 
 	# Splits fish name headers so they can add to DB seamlessly
 	def proper_headers(arr)
@@ -34,6 +42,17 @@ class LegacyCapture
 				write_crude << fix_csv
 			end
 		end
+	end
+
+	def extract_date(str)
+		Date.strptime(str, '%m/%d/%Y')
+	end
+
+	def extract_dam(dam_name)
+		dam = (Dam.where("LOWER(name) LIKE ?", dam_name.downcase).first).id
+	end
+
+	def extract_species_array
 	end
 
 end
