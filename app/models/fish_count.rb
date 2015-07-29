4290dc263ec8776a92fcd4a9f52a6f8066a923c8
Sillson <Sillson88@gmail.com>
2015-07-28 23:26:20 -0700

    IT'S ALIVE (the parser)

--- app/models/fish_count.rb
+++ app/models/fish_count.rb
@@ -4,6 +4,8 @@ class FishCount < ActiveRecord::Base
 
 	# Date validation will limit the feed to only pull in the first first per date.
 	# Will need to refactor in order to validate all three fields are unique (fish, dam, date)
+	validates :dam_id, presence: true
+	validates :fish_id, presence: true
 	validates :count_date, presence: true
 	validates :count_date, :uniqueness => { :scope => [:dam_id, :fish_id] }
 
--- lib/capture/legacy_capture.rb
+++ lib/capture/legacy_capture.rb
@@ -8,25 +8,28 @@ class LegacyCapture
 		crude = CSV.read("public/#{csv}.csv")
 		fix_csv(crude)
 		@report = CSV.read("public/ephemeral.csv", headers:true)
-		@dam = extract_dam(dam_name)
+		@dam_id = extract_dam(dam_name)
 	end
 
 	def parse_to_db
-		puts 'Hello'
-		puts "#{@report}"
-
+		@species = extract_species_array(@report)
 		@report.each do |fish_run|
 			@date = extract_date(fish_run["Date"])
-
-			begin
-				@new_count = FishCount.new(dam_id: @dam_id, count_date: @date, fish_id: @fish_id, count: @fish_count)
-				@new_count.save!
-			rescue ActiveRecord::RecordInvalid => invalid
-				puts invalid.record.errors
+			@species.each do |spc|
+				@fish_id = extract_fish(spc) 
+				@fish_count = (fish_run[spc]).to_i
+				begin
+					new_count = FishCount.new(dam_id: @dam_id, count_date: @date, fish_id: @fish_id, count: @fish_count)
+					new_count.save!
+				rescue ActiveRecord::RecordInvalid => invalid
+					puts invalid.record.errors
+				end
 			end
-
 		end
 	end
+end
+
+private
 
 	# Splits fish name headers so they can add to DB seamlessly
 	def proper_headers(arr)
@@ -44,15 +47,32 @@ class LegacyCapture
 		end
 	end
 
+	# Create a Date object from a string as provided by FPC
 	def extract_date(str)
 		Date.strptime(str, '%m/%d/%Y')
 	end
 
+	def extract_fish(str)
+		fish_id = (Fish.where("LOWER(name) LIKE ?", str.downcase).first).id
+		return fish_id
+	end
+
+	# Find a Dam object through a string
 	def extract_dam(dam_name)
-		dam = (Dam.where("LOWER(name) LIKE ?", dam_name.downcase).first).id
+		dam_id = (Dam.where("LOWER(name) LIKE ?", dam_name.downcase).first).id
+		return dam_id
 	end
 
-	def extract_species_array
+	# Build an array of all species counted by a certain dam
+	def extract_species_array(csv)
+		@collect = []
+		spc_arr = Fish.all.map {|fish| fish.name}
+		csv.headers.each do |read_header|
+			spc_arr.each do |read_fish|
+				if read_header == read_fish
+					@collect.push(read_header)
+				end
+			end
+		end
+		return @collect
 	end
-
-end
