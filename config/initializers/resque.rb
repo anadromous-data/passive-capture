b7a0df7944119d26336e111f112cf09027dcf33c
Sillson <Sillson88@gmail.com>
2015-08-08 00:07:19 -0700

    giving up on scheduler. going back to standard

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -8,6 +8,6 @@ ENV["REDISTOGO_URL"] ||= ENV["redistogo_url"]
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
-# Schedule Job #
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'daily_capture.yml'))
+# # Schedule Job #
+# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+# Resque.schedule = YAML.load_file(Rails.root.join('config', 'daily_capture.yml'))
