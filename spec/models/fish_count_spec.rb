983b42652be781ecc9b3fb9a95856ff00fbf95e6
Sillson <Sillson88@gmail.com>
2015-08-15 15:11:03 -0700

    I wrote a test

---
+++ spec/models/fish_count_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+
+describe FishCount do
+	it 'should find its previous count if exists, else return false' do
+		@first_count = FishCount.create!(dam_id: 4, count_date: Date.today, fish_id: 1, count: 33)
+    	@second_count = FishCount.create!(dam_id: 4, count_date: Date.today.ago(1.day), fish_id: 1, count: 66)
+    	@third_count = FishCount.create!(dam_id: 4, count_date: Date.today.ago(2.day), fish_id: 1, count: 99)
+
+    	expect(@first_count.last_count).to eq(66)
+    	expect(@second_count.last_count).to eq(99)
+    	expect(@third_count.last_count).to be false
+    end
+
+end
