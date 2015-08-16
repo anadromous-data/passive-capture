1da09e7d972705d703a69ffe9e53b60b0ab2f416
Sillson <Sillson88@gmail.com>
2015-08-15 22:52:18 -0700

    refactoring and including

--- app/controllers/dams_controller.rb
+++ app/controllers/dams_controller.rb
@@ -1,25 +1,23 @@
 class DamsController < ApplicationController
 
 	def index
-		# If including count/fish data, includes will perform better
 		@dams = Dam.all
 	end
 
 	def show
 		@dams = Dam.all
 		@dam = Dam.includes(:fish_counts, :fish).friendly.find(params[:id])
-		@fish = Fish.all.order(id: :asc)
-		@fish_counts = FishCount.for_year(Date.today.year).where("dam_id =?", @dam.id).limit(200).order(count_date: :desc).paginate(:page => params[:page], :per_page => 20)
+		@fish_counts = @dam.fish_counts.for_year(Date.today.year).limit(200).order(count_date: :desc).paginate(:page => params[:page], :per_page => 20)
 	end
 
 	def count_data
 		@dam = Dam.includes(:fish_counts, :fish).friendly.find(params[:dam_id])
-		fish_counts = FishCount.for_year(Date.today.year).where("dam_id =?", @dam.id).limit(15).order(count_date: :desc)
+		fish_counts = @dam.fish_counts.for_year(Date.today.year).limit(15).order(count_date: :desc)
 		@daily_total = fish_counts.where("count_date = ?", fish_counts.first.count_date)
 
 		respond_to do |format|
 			format.json {
-				render json: {:fish_counts => @daily_total}
+				render json: @daily_total.to_json(:include => :fish)
 			}
 		end
 	end
