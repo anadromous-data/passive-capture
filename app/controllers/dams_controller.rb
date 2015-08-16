857a6a4de3e5353c53aaf5f44fca5cd9f4be352d
Sillson <Sillson88@gmail.com>
2015-08-15 19:05:23 -0700

    ajax route is up for the graph

--- app/controllers/dams_controller.rb
+++ app/controllers/dams_controller.rb
@@ -11,4 +11,15 @@ class DamsController < ApplicationController
 		@fish_counts = FishCount.for_year(Date.today.year).where("dam_id =?", @dam.id).limit(200).order(count_date: :desc).paginate(:page => params[:page], :per_page => 20)
 	end
 
+	def count_data
+		@dam = Dam.includes(:fish_counts, :fish).friendly.find(params[:dam_id])
+		fish_counts = FishCount.for_year(Date.today.year).where("dam_id =?", @dam.id).limit(15).order(count_date: :desc)
+		@daily_total = fish_counts.where("count_date = ?", fish_counts.first.count_date)
+
+		respond_to do |format|
+			format.json {
+				render json: {:fish_counts => @daily_total}
+			}
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 	mount Resque::Server, :at => "/resque"
   
     root 'home#index'
-
     get 'about', to: 'home#about'
-    get 'data', to: 'home#data'
 
     resources :dams, only: [:index, :show] do
+    	get 'count_data', to: 'dams#count_data'
   	  resources :fish_counts, only: [:index, :show]
     end
 
